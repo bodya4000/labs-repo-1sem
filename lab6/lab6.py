4574fe360f7a011017f7ac5a09f3b0d745a4cc96
Bohdan <studiesbohdan@gmail.com>
2023-11-24 21:13:12 +0200

    added new tests and wriiten some logic for tests
    Here I did some updates in tests and lab5. I created test module file in the same folder as lab 5 because its a bit convinienr and did some changes in lab 5 cause it's wasn't made for tests

--- lab6/lab6.py
+++ lab6/lab6.py
@@ -1,13 +1,20 @@
+"""
+This module contains classes for converting time formats.
+"""
+
 from abc import ABC, abstractmethod
 
-def check_zeros(date):
-    str_date = str(date)
-    return "0" + str_date if len(str_date) == 1 else str_date
+
 def check_length_of_date_list(date):
+    """
+    raises TimeException if len of date is not 3
+    """
     if len(date) != 3:
         raise TimeException("Please follow this example -> 12:12:12")
 
-def check_date_items(date):
+
+def validating_date_items(date):
+    """validates items in list date"""
     for i, item in enumerate(date):
         num = int(item)
         if len(item) != 2:
@@ -22,49 +29,87 @@ def check_date_items(date):
             raise TimeException("Cannot handle negative seconds")
 
 
+def check_if_value_is_string(value):
+    """checks if value is string"""
+    if not isinstance(value, str):
+        raise TimeException("Cannot handle other types except str")
+
+
+def validating_seconds_date(num):
+    """validates num value"""
+    if num > 86399:
+        raise TimeException("Too much seconds")
+    if num < 0:
+        raise TimeException("Cannot handle negative seconds")
+
+
 class TimeException(Exception):
+    """Exception that is raised when there's unexpected"""
+
     def __init__(self, message="Це моя власна помилка!"):
         self.message = message
         super().__init__(self.message)
 
 
 class AbstractClass(ABC):
+    """this is abstract class"""
+
+    @abstractmethod
+    def hello(self):
+        """this method does greet"""
+        print("hello")
+
     @abstractmethod
     def convert_time(self, value):
-        pass
+        """abstract method to make polymorphism"""
 
 
-class TimeConverterToDate(AbstractClass):
+class TimeConverterToDate(AbstractClass, ABC):
+    """this is class that can convert seconds into date"""
+
+    def hello(self):
+        """this overriding method from abstract class"""
+        print("hello from TimeConverterToDate")
+
     def convert_time(self, value):
-        if value > 86399:
-            raise TimeException("Too much seconds")
-        if value < 0:
-            raise TimeException("Cannot handle negative seconds")
+        """Converts seconds into date"""
+        validating_seconds_date(value)
 
         hours = int(value / 3600)
         minutes = int((value - hours * 3600) / 60)
         seconds = value - hours * 3600 - minutes * 60
 
-        return f"{hours:02d}:{minutes:02d}:{seconds:02d}"  # f string padding
+        return f"{hours:02d}:{minutes:02d}:{seconds:02d}"  # Padding using f-string
 
 
-class TimeConverterToSeconds(AbstractClass):
+class TimeConverterToSeconds(AbstractClass, ABC):
+    """this is class that can convert date into seconds"""
     MULTIPLIERS = [3600, 60, 1]
 
+    def hello(self):
+        """this overriding method from abstract class"""
+
+        print("hello from TimeConverterToDate")
+
     def __init__(self):
+        """constructor"""
         self.__date = []
 
     def set_date(self, date):
+        """Setter"""
         check_length_of_date_list(date)
-        check_date_items(date)
+        validating_date_items(date)
         self.__date = date
 
     def get_date(self):
+        """getter"""
         return self.__date
 
-    def convert_time(self, date_str):
+    def convert_time(self, value: str):
+        """Converts the time from seconds to HH:MM:SS format."""
         total_seconds = 0
-        self.set_date(date_str.split(":"))
+        check_if_value_is_string(value)
+        self.set_date(value.split(":"))
 
         for i, item in enumerate(self.get_date()):
             num = int(item)
@@ -73,8 +118,9 @@ class TimeConverterToSeconds(AbstractClass):
         return total_seconds
 
 
-converter_to_date = TimeConverterToDate()
-converter_to_seconds = TimeConverterToSeconds()
+if __name__ == "__main__":
+    converter_to_date = TimeConverterToDate()
+    converter_to_seconds = TimeConverterToSeconds()
 
-converter_to_date.convert_time(36721)
-converter_to_seconds.convert_time("10:12:01")
+    print(converter_to_date.convert_time(36721))
+    print(converter_to_seconds.convert_time("10:12:01"))
---
+++ lab6/test_module.py
@@ -0,0 +1,67 @@
+"""this module tests lab6.py"""
+
+import pytest
+from lab6 import TimeException, TimeConverterToSeconds, TimeConverterToDate
+
+
+@pytest.fixture(name="test_converting_to_date_1")
+def test_converting_to_date__1():
+    """fixture of TimeConverterToDate"""
+    return TimeConverterToDate()
+
+
+@pytest.fixture(name="test_converter_to_seconds_2")
+def test_converter_to_seconds__2():
+    """fixture of TimeConverterToSeconds"""
+    return TimeConverterToSeconds()
+
+
+@pytest.mark.parametrize('seconds, error', [(86400, "Too much seconds"), (-1, "Cannot handle negative seconds")],)
+def test_negative_time_conversion_to_date(test_converting_to_date_1, seconds, error):
+    """negative test converter_to_date"""
+    with pytest.raises(TimeException, match=error):
+        test_converting_to_date_1.convert_time(seconds)
+
+
+test_cases_positive_to_date = [
+    (36721, "10:12:01"),
+    (3600, "01:00:00"),
+    (7200, "02:00:00"),
+    (18000, "05:00:00"),
+    (43200, "12:00:00")
+]
+
+
+@pytest.mark.parametrize('seconds, expected_time', test_cases_positive_to_date)
+def test_positive_time_conversion_to_date(test_converting_to_date_1, seconds, expected_time):
+    """positive test converter_to_date"""
+    assert test_converting_to_date_1.convert_time(seconds) == expected_time
+
+
+@pytest.mark.parametrize('date, error', [
+    ("25:00:00", "hours cannot be more than 23"),
+    ("23:00:000", "Two characters for item only"),
+    ("21:76:00", "seconds or minutes cannot be more than 59"),
+    ("21:00:99", "seconds or minutes cannot be more than 59"),
+    (True, "Cannot handle other types except str")
+])
+def test_negative_time_conversion_to_seconds(test_converter_to_seconds_2, date, error):
+    """negative test converter_to_seconds"""
+    with pytest.raises(TimeException, match=error):
+        test_converter_to_seconds_2.convert_time(date)
+
+
+test_cases_to_sec = [
+    ("10:12:01", 36721),
+    ("01:00:00", 3600),
+    ("02:00:00", 7200),
+    ("05:00:00", 18000),
+    ("12:00:00", 43200)
+]
+
+
+@pytest.mark.parametrize('time_str, expected_seconds', test_cases_to_sec)
+def test_positive_time_conversion_to_seconds(test_converter_to_seconds_2,
+                                             time_str, expected_seconds):
+    """positive test converter_to_seconds"""
+    assert test_converter_to_seconds_2.convert_time(time_str) == expected_seconds
